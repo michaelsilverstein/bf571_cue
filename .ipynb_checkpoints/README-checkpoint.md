93f369c7a42d6afd4aec96fef7de3f6fc2254755
Michael Silverstein <msilverstein@finchtherapeutics.com>
2021-03-29 12:19:00 -0400

    handle permissions error

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,4 @@
+# BF571 CUE
+
+1. [cue_tools.py](cue_tools.py): Shared script for functions
+2. [MS_CUE.ipynb](MS_CUE.ipynb): Michael's CUE analysis
--- cue_tools.py
+++ cue_tools.py
@@ -1,7 +1,19 @@
+import tempfile
+import cobra
+import requests
+import os
+
 def loadModelURL(url):
-    """Load SMBL cobra model from a URL"""
-    with tempfile.NamedTemporaryFile('w') as tf:
+    """
+    Load SMBL cobra model from a URL
+    
+    Example: 
+    url = 'http://bigg.ucsd.edu/static/models/e_coli_core.xml'
+    model = loadModelURL(url)
+    """
+    with tempfile.NamedTemporaryFile('w', delete=False) as tf:
         handle = requests.get(url)
         tf.write(handle.text)
         model = cobra.io.read_sbml_model(tf.name)
-        return model
+    os.remove(tf.name)
+    return model
